fbab7a97b65fe4280fa0e581abc143fdcda12dab
sienna <20200803@sungshin.ac.kr>
2022-11-09 10:07:12 +0900

    :tada:feat : 프로젝트 시작

---
+++ src/main/java/kr/co/msa_popool/MsaPopoolApplication.java
@@ -0,0 +1,13 @@
+package kr.co.msa_popool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaPopoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaPopoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/msa_popool/MsaPopoolApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.msa_popool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPopoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
