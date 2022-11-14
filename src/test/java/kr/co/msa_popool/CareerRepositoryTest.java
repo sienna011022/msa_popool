6f66bf62f51a8a0801a5fb75ded41a8f05a5027a
sienna <20200803@sungshin.ac.kr>
2022-11-14 23:58:14 +0900

    :white_check_mark:test : 인사 내역을 조회한다 - repository

---
+++ src/test/java/kr/co/msa_popool/CareerRepositoryTest.java
@@ -0,0 +1,48 @@
+package kr.co.msa_popool;
+
+import kr.co.msa_popool.career.domain.Career;
+import kr.co.msa_popool.career.domain.CareerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+
+public class CareerRepositoryTest {
+
+    @Mock
+    CareerRepository careerRepository;
+
+    @AfterEach
+    public void cleanup() {
+        careerRepository.deleteAll();
+    }
+
+    @Test
+    public void 인사내역불러오기() {
+        String memberId = "sienna1022";
+        when(careerRepository.findByMemberId(memberId))
+            .thenReturn(Optional.of(Career.builder()
+                .memberId(memberId)
+                .name("sienna")
+                .email("sienna011022@naver.com")
+                .period("0years")
+                .selfDescription("안녕하세요")
+                .build()));
+
+        Optional<Career> career = careerRepository.findByMemberId(memberId);
+
+        assertThat(career.get().getMemberId()).isEqualTo(memberId);
+
+    }
+
+
+}
