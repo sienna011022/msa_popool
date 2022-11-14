d30b0a8aa796ebb23f4d30678005c0eff4b0bfb5
sienna <20200803@sungshin.ac.kr>
2022-11-14 22:42:08 +0900

    :white_check_mark:test : 인사생성 예외 처리

--- src/test/java/kr/co/msa_popool/CareerServiceTest.java
+++ src/test/java/kr/co/msa_popool/CareerServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static kr.co.msa_popool.infra.ResponseCode.SUCCESS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 public class CareerServiceTest {
@@ -37,4 +38,24 @@ public class CareerServiceTest {
         ResponseFormat response = careerService.createCareer(careerRequest);
         assertThat(response.getCode()).isEqualTo(SUCCESS.getCode());
     }
+
+    @Test
+    @DisplayName("아이디를 입력하지 않으면 예외 발생")
+    public void career_생성_예외1() {
+        CareerCreateRequest errorRequest = new CareerCreateRequest(null, "김성윤", "sienna011022@naver.com", "0", "안녕하세요 백엔드 인턴을 준비중인 김성윤입니다");
+        assertThatThrownBy(() -> errorRequest.toCareer()).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("이름을 입력하지 않으면 예외 발생")
+    public void career_생성_예외2() {
+        CareerCreateRequest errorRequest = new CareerCreateRequest("sienna011022", null, "sienna011022@naver.com", "0", "안녕하세요 백엔드 인턴을 준비중인 김성윤입니다");
+        assertThatThrownBy(() -> errorRequest.toCareer()).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("아이디를 입력하지 않으면 예외 발생")
+    public void career_생성_예외3() {
+        CareerCreateRequest errorRequest = new CareerCreateRequest("sienna011022", "김성윤", null, "0", "안녕하세요 백엔드 인턴을 준비중인 김성윤입니다");
+        assertThatThrownBy(() -> errorRequest.toCareer()).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
