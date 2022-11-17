72f4bfd1a37cd2194d278d0a10a298b1ae1d08e6
sienna <20200803@sungshin.ac.kr>
2022-11-17 14:59:28 +0900

    :white_check_mark:test : 평가_등록_및_조회

---
+++ career/src/test/java/kr/co/msa_popool/score/application/ScoreServiceTest.java
@@ -0,0 +1,38 @@
+package kr.co.msa_popool.score.application;
+
+import kr.co.msa_popool.career.domain.CareerRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static java.util.Optional.of;
+import static kr.co.msa_popool.career.web.CareerFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ScoreServiceTest {
+
+    @Mock
+    CareerRepository careerRepository;
+
+    @InjectMocks
+    ScoreService scoreService;
+
+
+    @Test
+    @DisplayName("인사 내역을 등록하고, 평가자 아이디로 자신이 평가한 평가를 조회할 수 있다")
+    public void 평가_등록_및_조회() {
+
+        when(careerRepository.findByMemberId(MEMBER_ID))
+            .thenReturn(of(createCareer()));
+
+        scoreService.newScore(MEMBER_ID,createScore());
+
+        assertThat(scoreService.showScore(EVALUATOR_ID)).isEqualTo(createScoreInfo);
+
+    }
+}
